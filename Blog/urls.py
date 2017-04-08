4cc18b83a706517a6ee4c6ca95b85fe89ab0c0ca
jinkygaldones <jinkygaldones04@gmail.com>
2017-04-08 22:32:15 +0000

    Blog update

--- Blog/urls.py
+++ Blog/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
         url(r'^admin/', admin.site.urls),
-        url(r'^news/', include("news.urls", namespace='news')),
+        url(r'', include("news.urls", namespace='news')),
 ]
 
 if settings.DEBUG:
