220f36f07ab095e925f6d1cc7ddd2e19f055ec30
jinkygaldones <jinkygaldones04@gmail.com>
2017-04-08 22:22:49 +0000

    Blog update

--- Blog/wsgi.py
+++ Blog/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for Blog project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/Jinky/Jinky'  # use your own PythonAnywhere username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Blog.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blog.settings")
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
